8168f4f29b8acfca08f59e6e0239259b18f26698
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-28 00:42:41 -0300

    create global style

---
+++ src/styles/global.jsx
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+import { useTheme } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0px;
+    padding: 0px;
+    font-family: 'Fredoka', sans-serif;
+    /* font-weight: normal; */
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+}
+
+.Mui-selected:hover: {
+backgroundColor: 'red !important',
+},
+`;
