3f5a669919300ed21ab7cce70134b054286826d6
Crhistian <crhistiandeoliveira@gmail.com>
2022-03-03 01:04:33 -0300

    added now reducer

--- src/store/rootReducer.jsx
+++ src/store/rootReducer.jsx
@@ -1,9 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import config from './ConfigSlice';
-
+import modalExport from './ExportSlice';
 const createReducer = () => (state, action) => {
   const combinedReducer = combineReducers({
     config,
+    modalExport,
   });
 
   return combinedReducer(state, action);
