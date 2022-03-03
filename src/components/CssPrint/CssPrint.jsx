e5617590d685256a6f3182af0a25d21f42317a7e
Crhistian <crhistiandeoliveira@gmail.com>
2022-03-03 01:03:45 -0300

    create component to show css export

---
+++ src/components/CssPrint/CssPrint.jsx
@@ -0,0 +1,43 @@
+import { TextField } from '@mui/material';
+
+export const CssPrint = (props) => {
+  return (
+    <div
+      style={{
+        overflow: 'auto',
+        scrollbarWidth: 'thin',
+        maxHeight: '600px',
+        width: '430px',
+        margin: '15px 0px 25px 25px',
+      }}
+    >
+      <TextField
+        variant="standard"
+        multiline
+        spellCheck={false}
+        value={props.value}
+        sx={{
+          width: '400px',
+          '& .MuiInputBase-input': {
+            fontFamily: 'Fredoka',
+          },
+          '& .MuiInput-root': {
+            borderColor: 'transparent',
+            ':before': {
+              transition: 'border-bottom-color  86400s ',
+              borderColor: 'transparent',
+              ':hover': {
+                borderColor: 'transparent',
+              },
+            },
+            ':after': {
+              borderColor: 'transparent',
+            },
+          },
+        }}
+      >
+        {props.children}
+      </TextField>
+    </div>
+  );
+};
