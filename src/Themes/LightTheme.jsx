8ab6078837e5b9c70b6b3c930675f08b88b12453
Crhistian <crhistiandeoliveira@gmail.com>
2022-03-04 01:11:12 -0300

    create main theme

---
+++ src/Themes/LightTheme.jsx
@@ -0,0 +1,27 @@
+import { createTheme } from '@mui/material';
+
+const lightTheme = createTheme({
+  palette: {
+    background: {
+      default: '#f8f9fb',
+    },
+    primary: {
+      main: 'rgba(51, 51, 53, 1)',
+    },
+  },
+  typography: {
+    fontFamily: 'Fredoka',
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {},
+      },
+    },
+    MuiCssBaseline: {
+      styleOverrides: {},
+    },
+  },
+});
+
+export default lightTheme;
