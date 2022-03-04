f68f0628a3c8ffd03203cafe9a5e9ab079158076
Crhistian <crhistiandeoliveira@gmail.com>
2022-03-04 01:12:07 -0300

    clean old globalStyle

--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -1,21 +1,18 @@
-import { createGlobalStyle } from 'styled-components';
-import { useTheme } from 'styled-components';
+// import { createGlobalStyle } from 'styled-components';
+// import { useTheme } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
-* {
-    margin: 0px;
-    padding: 0px;
-    /* font-weight: normal; */
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
+// export const GlobalStyle = createGlobalStyle`
+// * {
+//     margin: 0px;
+//     padding: 0px;
+// }
 
-p, label, input {
-  font: normal;
-  font-family: 'Fredoka', sans-serif;
-}
+// p, label, input {
+//   font: normal;
+//   font-family: 'Fredoka', sans-serif;
+// }
 
-code {
-  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
-}
-`;
+// code {
+//   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+// }
+// `;
