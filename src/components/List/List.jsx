8b09337295446372badf5c70965f953216027ef2
Crhistian <crhistiandeoliveira@gmail.com>
2022-03-08 15:11:39 -0300

    corrige remoção de background color

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -72,6 +72,7 @@ export const List = (props) => {
       <InputLabel
         id="demo-multiple-name-label"
         sx={{
+          backgroundColor: 'white',
           maxWidth: '100%',
           padding: '0rem 0.5rem 0rem 0rem',
         }}
