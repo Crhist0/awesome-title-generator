374ab80d2fb5570412e1191d4a02f0c167f74db4
Crhistian <crhistiandeoliveira@gmail.com>
2022-03-08 19:30:40 -0300

    add color on styled h1 to show the selected color

--- src/components/Preview/styled.jsx
+++ src/components/Preview/styled.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 export const StyledH1 = styled.h1`
+  color: ${(props) => props.textColor};
   font-size: ${(props) => props.size}px;
   text-align: ${(props) => props.align};
   text-shadow: ${(props) => props.shadow};
