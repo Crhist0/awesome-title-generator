0d52c8ef38be55475e3a47da27ebbebd1401e936
Crhistian <crhistiandeoliveira@gmail.com>
2022-03-10 12:01:10 -0300

    add new redux to control start animation

--- src/store/rootReducer.jsx
+++ src/store/rootReducer.jsx
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import config from './ConfigSlice';
 import modalExport from './ExportSlice';
+import drawer from './DrawerSlice';
 const createReducer = () => (state, action) => {
   const combinedReducer = combineReducers({
     config,
     modalExport,
+    drawer,
   });
 
   return combinedReducer(state, action);
