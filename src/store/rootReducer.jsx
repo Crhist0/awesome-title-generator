adb4c75641f00588ffd84917569dc57fdd7055f8
Crhistian <crhistiandeoliveira@gmail.com>
2022-03-14 00:19:34 -0300

    add theme redux state

--- src/store/rootReducer.jsx
+++ src/store/rootReducer.jsx
@@ -2,8 +2,10 @@ import { combineReducers } from '@reduxjs/toolkit';
 import config from './ConfigSlice';
 import modalExport from './ExportSlice';
 import drawer from './DrawerSlice';
+import theme from './ThemeSlice';
 const createReducer = () => (state, action) => {
   const combinedReducer = combineReducers({
+    theme,
     config,
     modalExport,
     drawer,
