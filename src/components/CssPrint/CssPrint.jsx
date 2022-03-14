84bad2b2d6653259c1b2f388ae71739354861df8
Crhistian <crhistiandeoliveira@gmail.com>
2022-03-14 00:22:39 -0300

    setup a width size by export choice

--- src/components/CssPrint/CssPrint.jsx
+++ src/components/CssPrint/CssPrint.jsx
@@ -1,13 +1,45 @@
 import { TextField } from '@mui/material';
 
 export const CssPrint = (props) => {
+  // this component needs refactor to be responsive
+
+  let size = {
+    x: 0,
+    y: 0,
+  };
+  switch (props.mode) {
+    case 'csspure':
+      size = {
+        x: 30,
+        y: 35,
+      };
+      break;
+    case 'cssclass':
+      size = {
+        x: 30,
+        y: 35,
+      };
+      break;
+    case 'react':
+      size = {
+        x: 20,
+        y: 35,
+      };
+      break;
+    default:
+      size = {
+        x: 24,
+        y: 35,
+      };
+      break;
+  }
   return (
     <div
       style={{
         overflow: 'auto',
         scrollbarWidth: 'thin',
-        height: '35rem',
-        width: '20rem',
+        height: size.y + 'rem',
+        width: size.x + 'rem',
         margin: '15px 0px 25px 25px',
       }}
     >
@@ -17,11 +49,11 @@ export const CssPrint = (props) => {
         spellCheck={false}
         value={props.value}
         sx={{
-          width: '27vw',
+          width: size.x - 2 + 'rem',
           '& .MuiInput-root': {
             borderColor: 'transparent',
             ':before': {
-              transition: 'border-bottom-color  86400s ',
+              transition: 'border-bottom-color  86400s ', // sorry for that, it was late
               borderColor: 'transparent',
               ':hover': {
                 borderColor: 'transparent',
