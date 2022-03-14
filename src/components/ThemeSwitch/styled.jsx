dbdde01f86329f4c91793c019a0c1427b6bcb11e
Crhistian <crhistiandeoliveira@gmail.com>
2022-03-14 01:41:40 -0300

    edit theme switch style

--- src/components/ThemeSwitch/styled.jsx
+++ src/components/ThemeSwitch/styled.jsx
@@ -14,18 +14,18 @@ export const MaterialUISwitch = styled(Switch)(({ theme }) => ({
       transform: 'translateX(22px)',
       '& .MuiSwitch-thumb:before': {
         backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-          '#fff'
+          theme.palette.background.default
         )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
       },
       '& + .MuiSwitch-track': {
         opacity: 1,
-        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+        backgroundColor: '#aab4be',
       },
     },
   },
 
   '& .MuiSwitch-thumb': {
-    backgroundColor: '#6E6E72',
+    backgroundColor: theme.palette.primary.main,
     width: 32,
     height: 32,
     '&:before': {
@@ -45,7 +45,7 @@ export const MaterialUISwitch = styled(Switch)(({ theme }) => ({
   },
   '& .MuiSwitch-track': {
     opacity: 1,
-    backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+    backgroundColor: '#aab4be',
     borderRadius: 20 / 2,
   },
 }));
