ca365038acff11d49e255f9b1e8e0c9775b95eef
Crhistian <crhistiandeoliveira@gmail.com>
2022-03-14 00:30:59 -0300

    create ko-fi button component

---
+++ src/components/KofiButton/KofiButton.jsx
@@ -0,0 +1,19 @@
+export const KofiButton = () => {
+  return (
+    <>
+      <a href="https://ko-fi.com/S6S4BL87H" target="_blank" rel="noreferrer">
+        <div
+          style={{
+            border: '0px',
+            borderRadius: '12px',
+            height: '36px',
+            width: '36px',
+            backgroundImage: 'url(https://cdn.ko-fi.com/cdn/kofi2.png?v=3)',
+            backgroundSize: '180px',
+            backgroundPosition: '-6px',
+          }}
+        ></div>
+      </a>
+    </>
+  );
+};
